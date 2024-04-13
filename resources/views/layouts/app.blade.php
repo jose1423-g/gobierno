c4cc43417d00296b28c4776db08f9c7d83d9111d
unknown <cubobale1423@gmail.com>
2024-04-12 21:36:47 -0500

    upgrade secure request

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="descripcion" content="@yield('meta_description', 'default')">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title> @yield('title') </title>    
     @vite('resources/css/app.css')    
     @vite('resources/js/app.js')    
