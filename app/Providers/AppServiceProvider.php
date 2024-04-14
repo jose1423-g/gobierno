ef72c8c208812751c1b46a2c1be359bc02457c82
unknown <cubobale1423@gmail.com>
2024-04-14 14:56:42 -0500

    https appservices providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,7 +32,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->environment('production')) {
-            URL::forceSheme('https');
+            URL::forceScheme('https');
         }
         
         try {
